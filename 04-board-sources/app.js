890de7b5e8fb08848adf53a86fba276194a7b9e3
Yan1993-Dev <66394095+Yan1993-Dev@users.noreply.github.com>
2021-08-13 14:33:50 +0300

    Start

---
+++ 04-board-sources/app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board')
+const colors = ['#5E05AA', '#77E4F0', '#439951', '#F5E800', '#F770F3', '#4C89D0', '#E3421D', '#093D67']
+const SQUARE_NUMBER = 578
+
+for (let i = 0; i < SQUARE_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
